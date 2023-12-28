0debecec1a7e7ad91d894b8a7ec87af55d0dcf5d
son <152856285+je-s0n@users.noreply.github.com>
2023-12-28 21:06:02 +0900

    Update DefaultValue2.cpp

--- cp1/DefaultValue2.cpp
+++ cp1/DefaultValue2.cpp
@@ -1,5 +1,11 @@
 #include <iostream>
 
+/*
+매개변수의 디폴트 값은 오른쪽 매개변수의 디폴트 값부터 채우는 형태로 정의해야 함
+e.g.  int YourFunc(int num1, int num2, int num3 = 30) { ... }
+      int YourFunc(int num1, int num2 = 20, int num3 = 30) { ... }
+      int YourFunc(int num1 = 10, int num2 = 20, int num3 = 30) { ... }
+*/
 int Adder(int num1 = 1, int num2 = 2);
 
 int main(void) {
