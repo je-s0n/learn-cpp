edbd4e60dfd3a1fd22d09ace5b5b584590b9aa84
son <152856285+je-s0n@users.noreply.github.com>
2023-12-28 21:36:07 +0900

    Create UsingDcl3.cpp

---
+++ cp1/UsingDcl3.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std; // 이름공간 std에 선언된 모든 것에 대해 이름공간 지정의 생략 명령
+
+int main(void)
+{
+  int num=20;
+  cout<<"Hello World!"<<endl;
+  cout<<"Hello "<<"World!"<<endl;
+  cout<<num<<' '<<'A';
+  cout<<' '<<3.14<<endl;
+  return 0;
+}
