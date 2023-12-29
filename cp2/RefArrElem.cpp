656efac47939722e53fc8717518db5f0039c7482
son <152856285+je-s0n@users.noreply.github.com>
2023-12-29 11:51:54 +0900

    Create RefArrElem.cpp

---
+++ cp2/RefArrElem.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+/*
+참조자의 변수 범위는 배열요소도 포함됨
+*/
+int main(void) 
+{
+  int arr[3] = {1,3,5};
+  int &ref1=arr[0];
+  int &ref2=arr[1];
+  int &ref3=arr[2];
+
+  cout<<ref1<<endl;
+  cout<<ref2<<endl;
+  cout<<ref3<<endl;
+  return 0;
+}
