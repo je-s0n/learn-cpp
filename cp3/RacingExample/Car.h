50cffc3043fb8f6bac42c67f941248b2607759b2
son <152856285+je-s0n@users.noreply.github.com>
2023-12-29 16:09:43 +0900

    Update Car.h

--- cp3/RacingExample/Car.h
+++ cp3/RacingExample/Car.h
@@ -1,11 +1,10 @@
 // Car.h : 클래스 선언을 담음
-/*
-  다른 소스파일이 여러 번 include 했을 때 발생하는 문제를 미리 방지하기 위해서 사용함
-  ifdef :  define이 됐을 때 사용한다는 뜻
-  ifndef : define이 되지 않았을 때 선언한다는 뜻
-  define : 상수값을 지정하기 위한 예약어로, 매크로라고 부름
-  endif : ifdef, infdef 이 끝났음을 알림
-*/
+// 다른 소스파일이 여러 번 include 했을 때 발생하는 문제를 미리 방지하기 위해서 사용함
+// ifdef :  define이 됐을 때 사용한다는 뜻
+// ifndef : define이 되지 않았을 때 선언한다는 뜻
+// define : 상수값을 지정하기 위한 예약어로, 매크로라고 부름
+// endif : ifdef, infdef 이 끝났음을 알림
+
 #ifndef __CAR_H__
 #define __CAR_H__
 
