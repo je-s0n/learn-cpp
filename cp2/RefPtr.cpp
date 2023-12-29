d431aba9e1893a3ced43ac0d6746274cd528f096
son <152856285+je-s0n@users.noreply.github.com>
2023-12-29 11:59:39 +0900

    Create RefPtr.cpp

---
+++ cp2/RefPtr.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+/*
+포인터(*) : 주소값을 갖는 변수
+이중포인터(**) : 포인터 주소값을 갖는 변수
+*/
+int main(void)
+{
+  int num=12;
+  int *ptr=&num; // num의 주소값을 갖는 포인터 변수
+  int **dptr=&ptr; // 포인터 변수의 주소값을 갖는 이중포인터 변수
+
+  int &ref=num; // num 변수의 참조자 ref 선언
+  
+  // 포인터 변수 ptr의 참조자이므로, 변수 num에 저장된 값이 출력됨
+  int *(&pref)=ptr;
+  int **(&dpref)=dptr;
+
+  cout<<ref<<endl;
+  cout<<*pref<<endl; // pref는 포인터 변수 ptr의 참조자이므로, 변수 num에 저장된 값이 출력됨
+  cout<<**dpref<<endl; // dpref는 포인터 변수 dptr의 참조자이므로, 변수 num에 저장된 값이 출력됨
+  return 0;
+}
